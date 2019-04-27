fc63ebb5b5d0dc581a4dc5dc17f60acb14739b5e
xztaityozx <taityonohibi@gmail.com>
2019-04-27 17:46:49 +0900

    update

--- palette/palette.go
+++ palette/palette.go
@@ -26,8 +26,6 @@ func getColorTable(path string) []color.Color {
 	var r io.Reader
 	if len(path) == 0 {
 		r = base64.NewDecoder(base64.StdEncoding, strings.NewReader(srcImg))
-	} else if path == "ueda" {
-		r = base64.NewDecoder(base64.StdEncoding, strings.NewReader(uedaImg))
 	} else {
 		var err error
 		r, err = os.OpenFile(path, os.O_RDONLY, 0644)
@@ -108,13 +106,3 @@ innRi6y1vlGqzjFhy1WiPW2CE7OgbKY1uYhHlLhuqcMz8pPAcISQx82ttQkqdXEOCxnkO2qQn2+5
 OMb0qBZ1b5CicgHhmBGzqZjy0iLWf+0iNBdnyj1JWJxjfNDMut9HwvcDuW2hp935cItgf5QD6QQX
 dHn4VQY0GT6cbT00KoOUvhyh17KbMuEdHftSPNY1YZMF0NbfRGFssHP1QxCXKDH1dQMDmwzoouyt
 XEbxvRbHwhrTyb8sP3pNylzHaPM5bldYsa7UMPNgnv9Ye8bsLd6zuwAAAABJRU5ErkJggg==`
-
-const uedaImg = `iVBORw0KGgoAAAANSUhEUgAAABAAAAAICAYAAADwdn+XAAABx0lEQVQokQXBS0gUYQDA8f/3zXPX
-ncxUQouNRQ8lZGw3iSDoECEUEUqEdAnqHhh16eLJY0ngqfAQ9LCICA8qRRJFULRGtHjo4uJjXdvn
-7M7Mzsx+/X7i0dyMSkhJpdHCtjQcx8EyNEpll5HRUep7O6RSJtWWi2bZhCImaYDb0Lk2MYlu92bo
-sS28aB0zpfCigCeLq3x8v4wmDVAKpCLwYerWZaYmLqEiDWFEdASIOA5VuROT1E0ezk5zf3qGKKxw
-9eI4pgW1UgWpC5zeHp6/+4amCZ69ekxfv8P5s1fQ/Z08fUdOEqiYgcGjhF6dO7evk0hJHoyPkd+u
-MzI0jN90uTl5gaeLK9RdhW7HBO02QimlAMJOgaa7i1eEL2srnBZVpGZw2DmISiYpN6q0jp+hEZoU
-NrfIZgfpPpRBtls5OrT58FfyZrXAgYF+uswkS/PL5GsN1raKfPq1zt5+haET51i4ew/vxQLpY2P4
-LQ/9q5cjDn7gpLsJa5/5/XODzbnXREFEc7tEJpPm+1KOZk3xZ/4U2Thmf7dEsZTH803E7MYNJWwf
-9+U/ht9W6FICITTCjiT0I+IowtIECctAlwIpFIZlInVBApP/HeLIMN59qZEAAAAASUVORK5CYII=`
